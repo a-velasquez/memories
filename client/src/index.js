b657d224555800407aef5ebf3df5017d4f25762e
Alex Velazquez <avelasquez075@gmail.com>
2021-07-29 16:21:55 -0700

    import React, ReactDOM and App, render root

---
+++ client/src/index.js
@@ -0,0 +1,7 @@
+// where will connect our application to the index.html file
+import React from "react"
+import ReactDOM from "react-dom"
+import App from "./App"
+
+// connects do <div> with 'root' id
+ReactDOM.render(<App />, document.getElementById("root"))
