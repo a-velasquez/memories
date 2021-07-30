1e70cda6e96126a37545acb2c62b97e0f434ad05
Alex Velazquez <avelasquez075@gmail.com>
2021-07-29 22:26:13 -0700

    add mongoose db connection

--- server/index.js
+++ server/index.js
@@ -11,3 +11,15 @@ app.use(cors())
 
 const CONNECTION_URL =
 	"mongodb+srv://avelasquez:1234@cluster0.tssfh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+const PORT = process.env.PORT || 5000
+
+mongoose
+	// returns a promise
+	.connect(CONNECTION_URL, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true
+	})
+	.then(() =>
+		app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+	)
+	.catch((error) => console.log(error.message))
