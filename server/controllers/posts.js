8f2e89d90b2885cc95b52680c7570ea23aa62313
Alex Velazquez <avelasquez075@gmail.com>
2021-08-02 21:09:02 -0700

    add createPost fn and import PostMessage model

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,3 +1,9 @@
+import PostMessage from "../models/postMessage.js"
+
 export const getPosts = (req, res) => {
 	res.send("IT STILL WORKS")
+}
+
+export const createPost = (req, res) => {
+	res.send("Post Creation")
 }
