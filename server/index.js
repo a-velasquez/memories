e5873fd7c95c8718d5ce37347daca4175d109988
Alex Velazquez <avelasquez075@gmail.com>
2021-08-02 19:58:33 -0700

    finish connecting server to db

--- server/index.js
+++ server/index.js
@@ -15,11 +15,12 @@ const PORT = process.env.PORT || 5000
 
 mongoose
 	// returns a promise
-	.connect(CONNECTION_URL, {
-		useNewUrlParser: true,
-		useUnifiedTopology: true
-	})
+	.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
 	.then(() =>
-		app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+		app.listen(PORT, () =>
+			console.log(`Server Running on Port: http://localhost:${PORT}`)
+		)
 	)
-	.catch((error) => console.log(error.message))
+	.catch((error) => console.log(`${error} did not connect`))
+
+mongoose.set("useFindAndModify", false)
