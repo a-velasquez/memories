2fbfba0dac12c35347ffeb2f562a214fb4d94c78
Alex Velazquez <avelasquez075@gmail.com>
2021-08-04 15:28:50 -0700

    finish getPosts and createPost async await fns

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,9 +1,22 @@
 import PostMessage from "../models/postMessage.js"
 
-export const getPosts = (req, res) => {
-	res.send("IT STILL WORKS")
+export const getPosts = async (req, res) => {
+	try {
+		const postMessages = await PostMessage.find()
+		res.status(200).json(postMessages)
+	} catch (error) {
+		res.status(404).json({})
+	}
 }
 
-export const createPost = (req, res) => {
-	res.send("Post Creation")
+export const createPost = async (req, res) => {
+	const post = req.body
+	const newPost = new PostMessage(post)
+
+	try {
+		await newPost.save()
+		res.status(201).json(newPost)
+	} catch (error) {
+		res.status(409).json({ message: error.message })
+	}
 }
