bf7880b1c64791676052804fd51f8f878f36d21b
Alex Velazquez <avelasquez075@gmail.com>
2021-08-04 23:12:42 -0700

    import reducers, wrap App in Provider and add store

--- client/src/index.js
+++ client/src/index.js
@@ -5,5 +5,15 @@ import App from "./App"
 import { Provider } from "react-redux"
 import { createStore, applyMiddleware, compose } from "redux"
 import thunk from "redux-thunk"
+
+import reducers from "./reducers"
+
+const store = createStore(reducers, compose(applyMiddleware(thunk)))
+
 // connects do <div> with 'root' id
-ReactDOM.render(<App />, document.getElementById("root"))
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById("root")
+)
