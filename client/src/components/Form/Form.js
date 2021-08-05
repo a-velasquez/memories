fb5f337e4ba2dc0144d82c99b172f053f8b3b944
Alex Velazquez <avelasquez075@gmail.com>
2021-08-05 16:21:25 -0700

    add material-ui components, build basic form

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,9 +1,29 @@
 import React from "react"
+import { TextField, Button, Typography, Paper } from "@material-ui/core"
 import useStyles from "./styles"
 
 const Form = () => {
 	const classes = useStyles()
-	return <h1>Form Component</h1>
+
+	const handleSubmit = () => {}
+
+	return (
+		<Paper className={classes.paper}>
+			<form
+				autoComplete='off'
+				noValidate
+				className={classes.form}
+				onSubmit={handleSubmit}>
+				<Typography variant='h6'>Creating a Memory</Typography>
+				<TextField
+					name='creator'
+					variant='outlined'
+					label='Creator'
+					fullWidth
+				/>
+			</form>
+		</Paper>
+	)
 }
 
 export default Form
