b50941788e9229cb15357aae6691ca4843c624db
Alex Velazquez <avelasquez075@gmail.com>
2021-08-05 23:16:13 -0700

    add FileBase, submit/clear buttons, clear()

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react"
 import { TextField, Button, Typography, Paper } from "@material-ui/core"
+import FileBase from "react-file-base64"
 import useStyles from "./styles"
 
 const Form = () => {
@@ -13,6 +14,8 @@ const Form = () => {
 	const classes = useStyles()
 
 	const handleSubmit = () => {}
+
+	const clear = () => {}
 
 	return (
 		<Paper className={classes.paper}>
@@ -58,6 +61,32 @@ const Form = () => {
 					value={postData.tags}
 					onChange={(e) => setPostData({ ...postData, tags: e.target.value })}
 				/>
+				<div className={classes.fileInput}>
+					<FileBase
+						type='file'
+						multiple={false}
+						onDone={({ base64 }) =>
+							setPostData({ ...postData, selectedFile: base64 })
+						}
+					/>
+				</div>
+				<Button
+					className={classes.buttonSubmit}
+					variant='contained'
+					color='primary'
+					size='large'
+					type='submit'
+					fullWidth>
+					Submit
+				</Button>
+				<Button
+					variant='contained'
+					color='secondary'
+					size='small'
+					onClick={clear}
+					fullWidth>
+					Clear
+				</Button>
 			</form>
 		</Paper>
 	)
