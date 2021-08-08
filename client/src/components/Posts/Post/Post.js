3c1b2e8edd14e67a5a9d6a2549b64cb949b76dfc
Alex Velazquez <avelasquez075@gmail.com>
2021-08-07 22:19:52 -0700

    import icons, material comps, return Card, CardMedia

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -1,9 +1,29 @@
 import React from "react"
 import useStyles from "./styles"
+import {
+	Card,
+	CardActions,
+	CardContent,
+	CardMedia,
+	Button,
+	Typography
+} from "@material-ui/core"
+import ThumbUpAltIcon from "@material-ui/icons/ThumbUpAlt"
+import DeleteIcon from "@material-ui/icons/Delete"
+import MoreHorizIcon from "@material-ui/icons/MoreHoriz"
+import moment from "moment"
 
-const Post = () => {
+const Post = ({ post }) => {
 	const classes = useStyles()
-	return <h1>Post Component</h1>
+	return (
+		<Card className={classes.card}>
+			<CardMedia
+				className={classes.media}
+				image={post.selectedFile}
+				title={post.title}
+			/>
+		</Card>
+	)
 }
 
 export default Post
