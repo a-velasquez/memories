6c4c72445b7f548cad8c032ba03cf3cabbfd9974
Alex Velazquez <avelasquez075@gmail.com>
2021-08-09 16:53:45 -0700

    render 'Editing/Creating A Memory' conditionally

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -42,7 +42,9 @@ const Form = ({ currentId, setCurrentId }) => {
 				noValidate
 				className={`${classes.root} ${classes.form}`}
 				onSubmit={handleSubmit}>
-				<Typography variant='h6'>Creating a Memory</Typography>
+				<Typography variant='h6'>
+					{currentId ? "Editing" : "Creating"} a Memory
+				</Typography>
 				<TextField
 					name='creator'
 					variant='outlined'
@@ -74,10 +76,12 @@ const Form = ({ currentId, setCurrentId }) => {
 				<TextField
 					name='tags'
 					variant='outlined'
-					label='Tags'
+					label='Tags (coma separated)'
 					fullWidth
 					value={postData.tags}
-					onChange={(e) => setPostData({ ...postData, tags: e.target.value })}
+					onChange={(e) =>
+						setPostData({ ...postData, tags: e.target.value.split(",") })
+					}
 				/>
 				<div className={classes.fileInput}>
 					<FileBase
