83a5b70fec55ac3cee297a996459bbba5de2e8f2
Alex Velazquez <avelasquez075@gmail.com>
2021-08-09 14:30:01 -0700

    add useState, state values for post id, pass to Posts and Form

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -4,7 +4,7 @@ import { Grid, CircularProgress } from "@material-ui/core"
 import Post from "./Post/Post"
 import useStyles from "./styles"
 
-const Posts = () => {
+const Posts = ({ setCurrentId }) => {
 	const classes = useStyles()
 	const posts = useSelector((state) => state.posts)
 
@@ -20,7 +20,7 @@ const Posts = () => {
 			spacing={3}>
 			{posts.map((post) => (
 				<Grid key={post._id} item xs={12} sm={6} md={6}>
-					<Post post={post} />
+					<Post post={post} setCurrentId={setCurrentId} />
 				</Grid>
 			))}
 		</Grid>
