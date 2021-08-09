54cd3d1295f3605750a9c50a7007f7a3e051fc1f
Alex Velazquez <avelasquez075@gmail.com>
2021-08-09 16:54:28 -0700

    fix title and details of card

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -37,12 +37,15 @@ const Post = ({ post, setCurrentId }) => {
 				</Button>
 			</div>
 			<div className={classes.details}>
-				<Typography variant='body2' color='textSecondary'>
-					{post.tags.map((tag) => `#${tag}`)}
+				<Typography variant='body2' color='textSecondary' component='h2'>
+					{post.tags.map((tag) => `#${tag} `)}
 				</Typography>
 			</div>
+			<Typography className={classes.title} variant='h5' gutterBottom>
+				{post.title}
+			</Typography>
 			<CardContent>
-				<Typography className={classes.title} variant='h5' gutterBottom>
+				<Typography variant='h6' gutterBottom>
 					{post.message}
 				</Typography>
 			</CardContent>
