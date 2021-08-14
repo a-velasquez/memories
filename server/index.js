8b8c66b3e1efb74d0bce1ee762fa00e859d4aae0
Alex Velazquez <avelasquez075@gmail.com>
2021-08-13 20:23:10 -0700

    remove un-necessary code

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ app.use(cors())
 app.use("/posts", postRoutes)
 
 const CONNECTION_URL = process.env.CONNECTION_URL
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT
 
 mongoose
 	// returns a promise
