10fb0a0af781a44b58c15f2e4df463a48122719e
Alex Velazquez <avelasquez075@gmail.com>
2021-08-15 13:27:26 -0700

    and  Link to component Typography

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { AppBar, Typography } from "@material-ui/core"
+import { Link } from "react-router-dom"
 import useStyles from "./styles"
 import memories from "../../images/memories.png"
 
@@ -9,7 +10,12 @@ const Navbar = () => {
 	return (
 		<AppBar className={classes.appBar} position='static' color='inherit'>
 			<div className={classes.brandContainer}>
-				<Typography className={classes.heading} variant='h2' align='center'>
+				<Typography
+					// component={Link}
+					// to='/'
+					className={classes.heading}
+					variant='h2'
+					align='center'>
 					Memories
 				</Typography>
 				<img className={classes.image} src={memories} alt='icon' height='60' />
