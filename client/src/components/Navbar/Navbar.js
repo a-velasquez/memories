6abfb4423032d540891a857bca9dcc5170b5439b
Alex Velazquez <avelasquez075@gmail.com>
2021-08-15 13:43:34 -0700

    add conditional rendering of Log Out button if user=true

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,11 +1,12 @@
 import React from "react"
-import { AppBar, Typography } from "@material-ui/core"
+import { AppBar, Typography, Toolbar, Avatar, Button } from "@material-ui/core"
 import { Link } from "react-router-dom"
 import useStyles from "./styles"
 import memories from "../../images/memories.png"
 
 const Navbar = () => {
 	const classes = useStyles()
+	const user = null
 
 	return (
 		<AppBar className={classes.appBar} position='static' color='inherit'>
@@ -20,6 +21,29 @@ const Navbar = () => {
 				</Typography>
 				<img className={classes.image} src={memories} alt='icon' height='60' />
 			</div>
+			<Toolbar className={classes.toolbar}>
+				{user ? (
+					<div className={classes.profile}>
+						<Avatar
+							className={classes.purple}
+							alt={user.result.name}
+							src={user.result.imageUrl}>
+							{user.result.name.charAt(0)}
+						</Avatar>
+						<Typography className={classes.userName} variant='h6'>
+							{user.result.name}
+						</Typography>
+						<Button
+							variant='containe'
+							className={classes.logout}
+							color='secondary'>
+							Log Out
+						</Button>
+					</div>
+				) : (
+					<div></div>
+				)}
+			</Toolbar>
 		</AppBar>
 	)
 }
