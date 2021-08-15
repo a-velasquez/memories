c18f39fbe3748ba7d9d22eeed08ad23536a62fea
Alex Velazquez <avelasquez075@gmail.com>
2021-08-15 12:01:19 -0700

    move Typography and img into div

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -8,14 +8,12 @@ const Navbar = () => {
 
 	return (
 		<AppBar className={classes.appBar} position='static' color='inherit'>
-			<Typography className={classes.heading} variant='h2' align='center'>
-				Memories
-			</Typography>
-			<img
-				className={classes.image}
-				src={memories}
-				alt='memories'
-				height='60'></img>
+			<div className={classes.brandContainer}>
+				<Typography className={classes.heading} variant='h2' align='center'>
+					Memories
+				</Typography>
+				<img className={classes.image} src={memories} alt='icon' height='60' />
+			</div>
 		</AppBar>
 	)
 }
