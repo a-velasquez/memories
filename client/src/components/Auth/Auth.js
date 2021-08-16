52c6924e3493ce12ce67ca1fed58e6ee868416b6
Alex Velazquez <avelasquez075@gmail.com>
2021-08-15 22:58:03 -0700

    import icons, build container with avatar

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -1,7 +1,29 @@
 import React from "react"
+import {
+	Avatar,
+	Button,
+	Paper,
+	Grid,
+	Typography,
+	Container
+} from "@material-ui/core"
+import useStyles from "./styles"
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined"
 
 const Auth = () => {
-	return <div></div>
+	const classes = useStyles()
+	const isSignup = false
+
+	return (
+		<Container component='main' maxWidth='xs'>
+			<Paper className={classes.paper} elevation={3}>
+				<Avatar className={classes.avatar}>
+					<LockOutlinedIcon />
+				</Avatar>
+				<Typography variant='h6'>{isSignup ? "Sign In" : "Sign up"}</Typography>
+			</Paper>
+		</Container>
+	)
 }
 
 export default Auth
