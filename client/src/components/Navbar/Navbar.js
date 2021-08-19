6158b20ad940cdc4f96ebec55869a82b90cfc02f
Alex Velazquez <avelasquez075@gmail.com>
2021-08-18 18:28:27 -0700

    add useEffect, rerender nav, add logout(), set prifile to local storsge

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,13 +1,26 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { AppBar, Typography, Toolbar, Avatar, Button } from "@material-ui/core"
-import { Link } from "react-router-dom"
+import { useDispatch } from "react-redux"
+import { Link, useHistory } from "react-router-dom"
 import useStyles from "./styles"
 import memories from "../../images/memories.png"
 
 const Navbar = () => {
 	const classes = useStyles()
-	const user = null
+	const [user, setUser] = useState(JSON.parse(localStorage.getItem("profile")))
+	const dispatch = useDispatch()
+	const history = useHistory()
 
+	const logout = () => {
+		dispatch({ type: "LOGOUT" })
+		history.push("/")
+		setUser(null)
+	}
+
+	useEffect(() => {
+		const token = user?.token
+		setUser(JSON.parse(localStorage.getItem("profile")))
+	}, [])
 	return (
 		<AppBar className={classes.appBar} position='static' color='inherit'>
 			<div className={classes.brandContainer}>
@@ -36,7 +49,8 @@ const Navbar = () => {
 						<Button
 							variant='containe'
 							className={classes.logout}
-							color='secondary'>
+							color='secondary'
+							onClick={logout}>
 							Log Out
 						</Button>
 					</div>
