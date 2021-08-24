9f4fa48baba8216e9af075d9b71645ca707db407
Alex Velazquez <avelasquez075@gmail.com>
2021-08-24 16:34:33 -0700

    fix deprecated justify to justifyContent

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -19,7 +19,7 @@ const Home = () => {
 			<Container>
 				<Grid
 					container
-					justify='space-between'
+					justifyContent='space-between'
 					alignItems='stretch'
 					spacing={3}>
 					<Grid item xs={12} sm={7}>
