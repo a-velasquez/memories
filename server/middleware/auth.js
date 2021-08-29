bf66b260dc999f4acdbf28e2b48dbeea8ce19487
Alex Velazquez <avelasquez075@gmail.com>
2021-08-29 14:24:42 -0700

    add auth middleware, also to routes

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -3,25 +3,24 @@ import jwt from "jsonwebtoken"
 const auth = async (req, res, next) => {
 	try {
 		const token = req.headers.authorization.split(" ")[1]
-    const isCustomAuth = token.length < 500
-    
-    let decodedData
-    
-    if (token && isCustomAuth) {
-      decodedData = jwt.verify(tokem 'test')
+		const isCustomAuth = token.length < 500
 
-      req.userId = decodedData?.id
-    } else {
-      decodedData = jwt.decode(token)
+		let decodedData
 
-      req.userId = decodedData?.sub
-    }
+		if (token && isCustomAuth) {
+			decodedData = jwt.verify(token, "test")
 
-    next()
+			req.userId = decodedData?.id
+		} else {
+			decodedData = jwt.decode(token)
+
+			req.userId = decodedData?.sub
+		}
+
+		next()
 	} catch (error) {
 		console.log(error)
 	}
 }
 
 export default auth
- 
