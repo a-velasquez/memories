d14aa84e96e7f6a41168150cb7e8360e423986cf
Alex Velazquez <avelasquez075@gmail.com>
2021-08-30 10:41:55 -0700

    refactor handleSubmit

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -17,6 +17,7 @@ const Form = ({ currentId, setCurrentId }) => {
 	)
 	const classes = useStyles()
 	const dispatch = useDispatch()
+	const user = JSON.parse(localStorage.getItem("profile"))
 
 	useEffect(() => {
 		if (post) setPostData(post)
@@ -25,11 +26,11 @@ const Form = ({ currentId, setCurrentId }) => {
 	const handleSubmit = (e) => {
 		e.preventDefault()
 
-		if (currentId) {
-			dispatch(updatePost(currentId, postData))
+		if (currentId === 0) {
+			dispatch(createPost({ ...postData, name: user?.result?.name }))
 			clear()
 		} else {
-			dispatch(createPost(postData))
+			dispatch(updatePost(currentId, { ...postData, name: user?.result?.name }))
 			clear()
 		}
 	}
@@ -37,12 +38,21 @@ const Form = ({ currentId, setCurrentId }) => {
 	const clear = () => {
 		setCurrentId(null)
 		setPostData({
-			creator: "",
 			title: "",
 			message: "",
 			tags: "",
 			selectedFile: ""
 		})
+	}
+
+	if (!user?.result?.name) {
+		return (
+			<Paper className={classes.paper}>
+				<Typography variant='h6' align='center'>
+					Please Sign In to create your own memories and like other's memories.
+				</Typography>
+			</Paper>
+		)
 	}
 
 	return (
