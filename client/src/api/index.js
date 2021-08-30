348f100af620be643c014b9528dd3193ba1ebf51
Alex Velazquez <avelasquez075@gmail.com>
2021-08-30 10:44:15 -0700

    fix API interceptor causing JWT  malformed error

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -7,10 +7,11 @@ const API = axios.create({ baseURL: "http://localhost:5000" })
 
 API.interceptors.request.use((req) => {
 	if (localStorage.getItem("profile")) {
-		req.headers.Authorization = `Bearer ${JSON.parse(
-			localStorage.getItem("profile")
-		)}`
+		req.headers.Authorization = `Bearer ${
+			JSON.parse(localStorage.getItem("profile")).token
+		}`
 	}
+
 	return req
 })
 
