e56227452d201e1e337af7f1e42b63941bf8dea5
Alex Velazquez <avelasquez075@gmail.com>
2021-08-31 21:46:36 -0700

    remove notes cleanup code

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,26 +1,45 @@
+import express from "express"
 import mongoose from "mongoose"
+
 import PostMessage from "../models/postMessage.js"
+
+const router = express.Router()
 
 export const getPosts = async (req, res) => {
 	try {
 		const postMessages = await PostMessage.find()
+
 		res.status(200).json(postMessages)
 	} catch (error) {
-		res.status(404).json({})
+		res.status(404).json({ message: error.message })
+	}
+}
+
+export const getPost = async (req, res) => {
+	const { id } = req.params
+
+	try {
+		const post = await PostMessage.findById(id)
+
+		res.status(200).json(post)
+	} catch (error) {
+		res.status(404).json({ message: error.message })
 	}
 }
 
 export const createPost = async (req, res) => {
 	const post = req.body
-	const newPost = new PostMessage({
+
+	const newPostMessage = new PostMessage({
 		...post,
 		creator: req.userId,
 		createdAt: new Date().toISOString()
 	})
 
 	try {
-		await newPost.save()
-		res.status(201).json(newPost)
+		await newPostMessage.save()
+
+		res.status(201).json(newPostMessage)
 	} catch (error) {
 		res.status(409).json({ message: error.message })
 	}
@@ -48,13 +67,15 @@ export const deletePost = async (req, res) => {
 
 	await PostMessage.findByIdAndRemove(id)
 
-	res.json({ message: "Post successfuly deleted!" })
+	res.json({ message: "Post deleted successfully." })
 }
 
 export const likePost = async (req, res) => {
 	const { id } = req.params
 
-	if (!req.userId) return res.json({ message: "Unauthorized Action" })
+	if (!req.userId) {
+		return res.json({ message: "Unauthenticated" })
+	}
 
 	if (!mongoose.Types.ObjectId.isValid(id))
 		return res.status(404).send(`No post with id: ${id}`)
@@ -64,16 +85,14 @@ export const likePost = async (req, res) => {
 	const index = post.likes.findIndex((id) => id === String(req.userId))
 
 	if (index === -1) {
-		// like the post
 		post.likes.push(req.userId)
 	} else {
-		// dislike the post
-		post.likes = post.likes.filter((id) => id != String(req.userId))
+		post.likes = post.likes.filter((id) => id !== String(req.userId))
 	}
-
 	const updatedPost = await PostMessage.findByIdAndUpdate(id, post, {
 		new: true
 	})
-
-	res.json(updatePost)
+	res.status(200).json(updatedPost)
 }
+
+export default router
