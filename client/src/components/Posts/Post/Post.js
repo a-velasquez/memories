97f7ad9d0feacf5c3c21e6e5fb6d49889c460d68
Alex Velazquez <avelasquez075@gmail.com>
2021-09-06 23:49:38 -0700

    reorder code, npm install

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -1,5 +1,4 @@
 import React from "react"
-import useStyles from "./styles"
 import {
 	Card,
 	CardActions,
@@ -7,22 +6,24 @@ import {
 	CardMedia,
 	Button,
 	Typography
-} from "@material-ui/core"
+} from "@material-ui/core/"
 import ThumbUpAltIcon from "@material-ui/icons/ThumbUpAlt"
-import ThumbUpAltOutlined from "@material-ui/icons/ThumbUpAltOutlined"
 import DeleteIcon from "@material-ui/icons/Delete"
 import MoreHorizIcon from "@material-ui/icons/MoreHoriz"
-import moment from "moment"
+import ThumbUpAltOutlined from "@material-ui/icons/ThumbUpAltOutlined"
 import { useDispatch } from "react-redux"
-import { deletePost, likePost } from "../../../actions/posts"
+import moment from "moment"
+
+import { likePost, deletePost } from "../../../actions/posts"
+import useStyles from "./styles"
 
 const Post = ({ post, setCurrentId }) => {
-	const classes = useStyles()
 	const dispatch = useDispatch()
+	const classes = useStyles()
 	const user = JSON.parse(localStorage.getItem("profile"))
 
 	const Likes = () => {
-		if (post.likes.length > 0) {
+		if (post?.likes?.length > 0) {
 			return post.likes.find(
 				(like) => like === (user?.result?.googleId || user?.result?._id)
 			) ? (
@@ -53,7 +54,10 @@ const Post = ({ post, setCurrentId }) => {
 		<Card className={classes.card}>
 			<CardMedia
 				className={classes.media}
-				image={post.selectedFile}
+				image={
+					post.selectedFile ||
+					"https://user-images.githubusercontent.com/194400/49531010-48dad180-f8b1-11e8-8d89-1e61320e1d82.png"
+				}
 				title={post.title}
 			/>
 			<div className={classes.overlay}>
@@ -78,7 +82,11 @@ const Post = ({ post, setCurrentId }) => {
 					{post.tags.map((tag) => `#${tag} `)}
 				</Typography>
 			</div>
-			<Typography className={classes.title} variant='h5' gutterBottom>
+			<Typography
+				className={classes.title}
+				gutterBottom
+				variant='h5'
+				component='h2'>
 				{post.title}
 			</Typography>
 			<CardContent>
--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -6,25 +6,23 @@ import {
 	LIKE
 } from "../constants/actionTypes"
 
-const reducer = (posts = [], action) => {
+export default (posts = [], action) => {
 	switch (action.type) {
+		case FETCH_ALL:
+			return action.payload
 		case LIKE:
+			return posts.map((post) =>
+				post._id === action.payload._id ? action.payload : post
+			)
+		case CREATE:
+			return [...posts, action.payload]
+		case UPDATE:
 			return posts.map((post) =>
 				post._id === action.payload._id ? action.payload : post
 			)
 		case DELETE:
 			return posts.filter((post) => post._id !== action.payload)
-		case UPDATE:
-			return posts.map((post) =>
-				post._id === action.payload._id ? action.payload : post
-			)
-		case FETCH_ALL:
-			return action.payload
-		case CREATE:
-			return [...posts, action.payload]
 		default:
 			return posts
 	}
 }
-
-export default reducer
