8480b63b0e6ceb734c5c331ec77d627c10a0b9b5
Alex Velazquez <avelasquez075@gmail.com>
2021-09-12 01:16:50 -0700

    add Paginatation.jsx starter code

---
+++ client/src/components/Pagination.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+import { Pagination, PaginationItem } from "@material-ui/lab"
+
+import useStyles from "./styles"
+
+const Paginate = () => {
+	return <Pagination />
+}
+
+export default Paginate
