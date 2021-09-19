d5568ee435706d0efea100e8364dee3f14f17e1e
Alex Velazquez <avelasquez075@gmail.com>
2021-09-19 15:53:13 -0700

    add routes for posts, posts/:id, posts/search

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from "react"
-import { BrowserRouter, Switch, Route } from "react-router-dom"
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom"
 import { Container } from "@material-ui/core"
+import PostDetails from "./components/PostDetails/PostDetails"
 import Navbar from "./components/Navbar/Navbar"
 import Home from "./components/Home/Home"
 import Auth from "./components/Auth/Auth"
@@ -8,10 +9,13 @@ import Auth from "./components/Auth/Auth"
 const App = () => {
 	return (
 		<BrowserRouter>
-			<Container maxWidth='lg'>
+			<Container maxWidth='xl'>
 				<Navbar />
 				<Switch>
-					<Route exact path='/' component={Home} />
+					<Route path='/' exact component={() => <Redirect to='/posts' />} />
+					<Route path='/posts' exact component={Home} />
+					<Route path='/posts/search' exact component={Home} />
+					<Route path='/posts/:id' exact component={PostDetails} />
 					<Route exact path='/auth' component={Auth} />
 				</Switch>
 			</Container>
